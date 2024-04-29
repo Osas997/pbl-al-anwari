b2d2a90f4a7096a98a7403071a86b607969378bc
athallah21 <athallahravi21@gmail.com>
2024-04-29 20:17:50 +0700

    add bank santri migrations

---
+++ database/migrations/2024_04_29_195125_create_bank_santris_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bank_santris', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_santri');
+            $table->string('nama_bank');
+            $table->string('sandi_bank');
+            $table->string('nama_rekening');
+            $table->string('nomor_rekening');
+            $table->timestamps();
+            $table->foreign('id_santri')->references('id')->on('santri');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bank_santris');
+    }
+};
