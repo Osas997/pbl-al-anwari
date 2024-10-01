9d725dd736e8da45bb8878a51e4d3857b6eec020
athallah21 <athallahravi21@gmail.com>
2024-10-01 20:33:11 +0700

    feat : add configuration coverage

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,10 +10,6 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
+    // php artisan test --coverage
 
-        $response->assertStatus(200);
-    }
 }
